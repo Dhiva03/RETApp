3929480726afa559a64eced2db5974285c2b9a2b
Dhiva04 <dhivakaran.ret@gmail.com>
2017-09-16 22:27:34 +0530

    server

--- config/db.js
+++ config/db.js
@@ -3,5 +3,5 @@ const crypto = require('crypto').randomBytes(256).toString('hex');
 module.exports={
  uri:'mongodb://ret:dk231425@ds139984.mlab.com:39984/ret_face',
  secret:'happy',
- db:'faceApp'
+ db:'ret_face'
 }
