4beb48263c36be893fa85e6e28cd6a10f6509cfb
Dhiva04 <dhivakaran.ret@gmail.com>
2017-09-16 20:57:10 +0530

    server change

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use(express.static(__dirname+'/public'));
 app.use('/register',register);
 
 app.get('*', (req,res) => {
-	res.sendFile(path.join(__dirname+'/client/dist/index.html'));
+	res.sendFile(path.join(__dirname+'/public/index.html'));
 });
 
 app.listen(port,()=> {
